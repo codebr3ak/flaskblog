65bed92201af5a03c91038dbc25d8fc208866221
codebr3ak <barakaralph@gmail.com>
2022-01-25 12:06:49 +0300

    Updated the main file structure

--- flaskblog.py
+++ flaskblog.py
@@ -1,13 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask (__name__)
 
-@app.route("/")
-def home():
-    return render_template(home.html)
+posts = [
+{
+    'author': 'Ralph Baraka',
+    'title': 'Blog post 1',
+    'content': 'First post content',
+    'date_posted': 'April 21, 2020'
+},
+{
+    'author': 'Peter Issa',
+    'title': 'Blog post 2',
+    'content': 'Second post content',
+    'date_posted': 'April 22, 2020'
+}
+]
 
 @app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/")
+@app.route("/about")
 def about():
-    return render_template(about.html)    
+    return render_template('about.html', title='About')    
 
 
 if __name__ == '__main__':
