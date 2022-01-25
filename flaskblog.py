2cc9179edc0c4d950feb28656af31f513d415e33
codebr3ak <barakaralph@gmail.com>
2022-01-25 11:23:06 +0300

    Added templates and defined routes

--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask (__name__)
 
 @app.route("/")
-def hello():
-    return "Hello world!"
+def home():
+    return render_template(home.html)
+
+@app.route("/")
+def about():
+    return render_template(about.html)    
 
 
 if __name__ == '__main__':
