c8cdb0ee64fd1b9216cc65d9808e13c2a2f70597
codebr3ak <barakaralph@gmail.com>
2022-01-27 15:27:39 +0300

    where I'm at currently, still debbuging the errors

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -6,8 +6,9 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f80930995d450ff7107857a3aff66b65'
 app.config['SQLALCHEMY_ DATABASE_URI'] = 'sqlite:///site.db'
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-# login_manager = LoginManager(app)
+
 
 from flaskblog import routes
