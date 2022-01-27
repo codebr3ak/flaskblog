ec6c19916f2f6a64d75e76f36d7b718330ef5e2f
codebr3ak <barakaralph@gmail.com>
2022-01-27 13:25:39 +0300

    made a change, program should run correctly

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -34,7 +34,7 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decede('utf-8')
-        user = user(username=form.username.data, email=form.email.data, password=hashed_password)
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash(f'Your account has been created! You can now login', 'success')
