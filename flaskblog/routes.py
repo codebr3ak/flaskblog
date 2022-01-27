d165a5ea87957fd81778f0cc5fd9e586f7a9b748
codebr3ak <barakaralph@gmail.com>
2022-01-27 14:11:03 +0300

    fixing the traceback error

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -33,7 +33,7 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decede('utf-8')
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
